bda5f3a475f468f110585e3b29fdae6c7104c783
Kai Chu <kai.chu@aphelion.se>
2017-01-12 09:31:08 +0100

    EffectiveJava: Item 7 fix typo

--- effective.java/effective/java/item7/Finalizer/Finalizer.java
+++ effective.java/effective/java/item7/Finalizer/Finalizer.java
@@ -1,5 +1,15 @@
 package effective.java.item7.Finalizer;
 
 public class Finalizer {
-
+	
+	Object bean = new Object();
+	
+	@Override
+	protected void finalize() throws Throwable {
+		try {
+			bean = null;
+		}finally {
+			super.finalize();
+		}
+	}
 }
